24842503f494718dd986448fa8d2a6473e71c93b
andres-vizcaino <andres.vizcaino@igloolab.co>
2023-10-19 14:09:18 -0500

    add support dynamic ip

--- src/app/api/info/route.ts
+++ src/app/api/info/route.ts
@@ -3,9 +3,20 @@ import { type NextRequest } from 'next/server'
 
 export async function GET (request: NextRequest) {
   // Get the IP address from the request
+  const ip = (request.headers.get('x-real-ip') != null) || request.headers.get('x-forwarded-for')
 
-  console.log(request.headers)
-  const res = await fetch(`https://ipinfo.io/181.140.249.239?token=${TOKEN_IPINFO}`)
+  // if development, use the IP address from the request
+  if (process.env.NODE_ENV === 'development') {
+    return Response.json({
+      data: {
+        city: 'Cartagena',
+        region: 'Bolívar',
+        country: 'CO'
+      }
+    })
+  }
+
+  const res = await fetch(`https://ipinfo.io/${ip}?token=${TOKEN_IPINFO}`)
   const data = await res.json()
 
   return Response.json({
